491f51b3fc1ee6f194c2164b65acdebb634ae63f
dimicuss <dimicuss@gmail.com>
2024-09-14 15:39:43 +0300

    feat: minor improvements

--- sprint_5/b.js
+++ sprint_5/b.js
@@ -11,81 +11,76 @@ if (process.env.REMOTE_JUDGE !== 'true') {
     }
   }
 
-  function parseNumber(str) {
-    return str === 'None' ? null : Number(str)
-  }
-
   function generateTree(tree) {
-    const [_, ...nodes] = tree.split('\n')
-    const parsedNodes = nodes.map((str) => {
-      const [_, valueStr, leftStr, rightStr] = str.split(' ')
-      const node = new CNode(parseNumber(valueStr))
-      return [node, () => {
-        const left = parseNumber(leftStr)
-        const right = parseNumber(rightStr)
+    const result = new Map()
+    const functions = []
 
-        if (left) {
-          node.left = parsedNodes[left][0]
+    tree.forEach(([id, value, left, right]) => {
+      const node = new CNode(value)
+
+      functions.push(() => {
+        if (left !== undefined) {
+          node.left = result.get(left)
         }
 
-        if (right) {
-          node.right = parsedNodes[right][0]
+        if (right !== undefined) {
+          node.right = result.get(right)
         }
-      }]
+      })
+
+      result.set(id, node)
     })
 
-    parsedNodes.forEach(([_, fn]) => fn())
+    functions.forEach((fn) => fn())
 
-    return parsedNodes[0][0]
+    return result.get(0)
   }
 
   (function suiteA() {
-    const tree = `6
-0 0 1 2
-1 2 None None
-2 7 3 4
-3 4 5 None
-4 8 None None
-5 12 None None`
-    const root = generateTree(tree)
+    const root = generateTree([
+      [0, 0, 1, 2],
+      [1, 2],
+      [2, 7, 3, 4],
+      [3, 4, 5],
+      [4, 8],
+      [5, 12]
+    ])
     console.assert(!solution(root), 'A')
   })();
 
 
   (function suiteB() {
-    const tree = `5
-0 1 1 2
-1 2 None None
-2 0 3 4
-3 3 None None
-4 6 None None`
-
-    const root = generateTree(tree)
+    const root = generateTree([
+      [0, 1, 1, 2],
+      [1, 2],
+      [2, 0, 3, 4],
+      [3, 3],
+      [4, 6]
+    ])
     console.assert(solution(root), 'B')
   })();
 
   (function suiteD() {
-    const tree = `9
-0 0 1 2
-1 1 3 None
-2 2 None 4
-3 3 5 6
-4 4 7 8
-5 5 None None
-6 6 None None
-7 7 None None
-8 8 None None`
-    const root = generateTree(tree)
+    const root = generateTree([
+      [0, 0, 1, 2],
+      [1, 1, 3],
+      [2, 2, , 4],
+      [3, 3, 5, 6],
+      [4, 4, 7, 8],
+      [5, 5],
+      [6, 6],
+      [7, 7],
+      [8, 8]
+    ])
     console.assert(!solution(root), 'D')
   })();
 
   (function suiteC() {
-    const tree = `3
-0 1 1 2
-1 2 None None
-2 0 None None`
-
-    const root = generateTree(tree)
+    const root = generateTree([
+      [0, 1, 1, 2],
+      [1, 2],
+      [2, 0]
+    ])
     console.assert(solution(root), 'C')
   })();
 }
