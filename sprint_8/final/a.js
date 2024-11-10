5c5a5bafb8a7fd8d2d3ca260903e1978e9e54c9b
dimicuss <dimicuss@gmail.com>
2024-11-10 18:24:57 +0300

    feat: add final task

---
+++ sprint_8/final/a.js
@@ -0,0 +1,97 @@
+const readline = require('readline')
+
+const reader = readline.createInterface({
+  input: process.stdin
+})
+
+let line = 0
+const lines = []
+
+reader.on('line', line => {
+  lines.push(line);
+})
+
+process.stdin.on('end', solve)
+
+const readLine = () => {
+  return lines[line++]
+}
+
+const readLines = () => {
+  let n = Number(readLine())
+  const result = []
+
+  while (n--) {
+    result.push(readLine())
+  }
+
+  return result
+}
+
+
+const digitMap = {1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true, 0: true}
+const isDigit = (char) => digitMap[char] || false
+
+const unpackString = (string) => {
+  let i = 0
+  const callStack = [{multiplier: 1, string: ''}]
+
+  let currentDigit = ''
+  while (callStack.length > 0 && i < string.length) {
+    const char = string[i]
+
+    if (isDigit(char)) {
+      currentDigit += char
+    } else if (char === '[') {
+      callStack.push({
+        multiplier: Number(currentDigit),
+        string: ''
+      })
+      currentDigit = ''
+    } else if (char === ']') {
+      const braceToClose = callStack.pop()
+      const currentBrace = callStack[callStack.length - 1]
+      currentBrace.string += braceToClose.string.repeat(braceToClose.multiplier)
+    } else {
+      const currentBrace = callStack[callStack.length - 1]
+      currentBrace.string += char
+    }
+
+    i++
+  }
+
+  return callStack[callStack.length - 1].string
+}
+
+const maxCommonPrefix = (strings) => {
+  let i = 0
+  let result = ''
+
+  mainLoop: while (true) {
+    const previousSymbol = strings[0][i]
+
+    if (previousSymbol === undefined) {
+      break mainLoop
+    }
+
+    for (let j = 1; j < strings.length; j++) {
+      const nextSymbol = strings[j][i]
+      if (previousSymbol !== nextSymbol || nextSymbol === undefined) {
+        break mainLoop
+      }
+    }
+
+    result += previousSymbol
+    i++
+  }
+
+  return result
+}
+
+function solve() {
+  const packedStrings = readLines()
+
+  console.log(
+    maxCommonPrefix(packedStrings.map(unpackString))
+  )
+}
