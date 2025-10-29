e900c39c5b59eaa300400d85be1448a6bc78a7da
dimicuss <dimicuss@gmail.com>
2025-10-29 15:02:22 +0300

    feat: practice

--- misk/dance-dance.js
+++ misk/dance-dance.js
@@ -1,50 +1,158 @@
+const u = require("util");
+
+const log = (obj) => {
+  console.log(u.inspect(obj, { colors: true, depth: null }));
+};
+
+const createMatrix = (str) => {
+  const mtx = [];
+
+  const rows = str.split("\n");
+  let start;
+
+  for (let i = 0; i < rows.length; i++) {
+    const row = [];
+
+    for (let j = 0; j < rows[i].length; j++) {
+      const direction = rows[i][j];
+
+      if (direction === "S") {
+        start = [j, i];
+      }
+
+      row.push(direction);
+    }
+
+    mtx.push(row);
+  }
+
+  return { mtx, start };
+};
+
 const dirDegMap = {
-    "→": 0,
-    "↗": 45,
-    "↑": 90,
-    "↖": 135,
-    "←": 180,
-    "↙": 225,
-    "↓": 270,
-    "↘": 315,
+  S: 0,
+  "→": 0,
+  "↗": 45,
+  "↑": 90,
+  "↖": 135,
+  "←": 180,
+  "↙": 225,
+  "↓": 270,
+  "↘": 315,
+};
+
+const degDirMap = {
+  0: "→",
+  45: "↗",
+  90: "↑",
+  135: "↖",
+  180: "←",
+  225: "↙",
+  270: "↓",
+  315: "↘",
 };
 
 const degCoordMap = {
-    0: [1, 0],
-    45: [1, 1],
-    90: [0, 1],
-    135: [-1, 1],
-    180: [-1, 0],
-    225: [-1, -1],
-    270: [0, -1],
-    315: [1, -1],
+  0: [1, 0],
+  45: [1, -1],
+  90: [0, -1],
+  135: [-1, -1],
+  180: [-1, 0],
+  225: [-1, 1],
+  270: [0, 1],
+  315: [1, 1],
 };
 
-const mod = (n, m) => {
-    const result = n < 0 ? Math.abs(m + n) % m : n % m;
-    return result;
+const mod = (n, m) => (n < 0 ? Math.abs(m + n) % m : n % m);
+
+const key = (x, y) => [x, y].join("|");
+
+const createCoordsGetter = (degrees) => (mtx, x, y) => {
+  const dir = mtx[y][x];
+
+  const dirDeg = dirDegMap[dir];
+  const map = new Map();
+  const list = [];
+
+  for (const deg of degrees) {
+    const nextDeg = mod(dirDeg + deg, 360);
+    const [xD, yD] = degCoordMap[nextDeg];
+
+    const nextX = x + xD;
+    const nextY = y + yD;
+
+    list.push([nextX, nextY, degDirMap[nextDeg]]);
+
+    if (!map.has(nextX)) {
+      map.set(nextX, new Set());
+    }
+
+    map.get(nextX).add(nextY);
+  }
+
+  return { list, map };
 };
 
-const createCoordGetter = (directions) => (dir) => {
-    const dirDeg = dirDegMap[dir];
+const getNextCoords = createCoordsGetter([90, 45, 0, -45, -90]);
+const getStartCoords = createCoordsGetter([135, 90, 45, 0, -45, -90, -135, -180]);
+const getRestrictedCoords = createCoordsGetter([45, 0, -45]);
 
-    const result = directions.map(
-        (incr) => degCoordMap[mod(dirDeg + incr, 360)],
-    );
+const dance_ = (mtx, current, cb, visited = {}, result = "") => {
+  const [x, y] = current;
+  const { list } = mtx[y][x] === "S" ? getStartCoords(mtx, x, y) : getNextCoords(mtx, x, y);
 
-    return result;
+  const nextVisited = {
+    ...visited,
+    [key(x, y)]: true,
+  };
+
+  for (const [nextX, nextY, nextDir] of list) {
+    let nextPlate;
+    nextPlate = (nextPlate = mtx[nextY]) && nextPlate[nextX];
+
+    if (nextPlate) {
+      const { map } = getRestrictedCoords(mtx, nextX, nextY);
+      const inRestricted = map.has(x) && map.get(x).has(y);
+
+      if (nextPlate === "S") {
+        cb(result + nextDir);
+      }
+
+      if (!inRestricted && !nextVisited[key(nextX, nextY)]) {
+        dance_(mtx, [nextX, nextY], cb, nextVisited, result + nextDir);
+      }
+    }
+  }
 };
 
-const getNextCoords = createCoordGetter([90, 45, 0, -45, -90]);
-const getRestrictedCoords = createCoordGetter([-45, 0, 45]);
+const dance = (map) => {
+  const { mtx, start } = createMatrix(map);
 
-console.log(
-    Object.keys(dirDegMap).map((dir) => [dir, getNextCoords(dir).join("|")]),
-);
+  let maxResult = "";
 
-console.log(
-    Object.keys(dirDegMap).map((dir) => [
-        dir,
-        getRestrictedCoords(dir).join("|"),
-    ]),
-);
+  dance_(mtx, start, (result) => {
+    if (result.length > maxResult.length) {
+      maxResult = result;
+    }
+  });
+
+  return maxResult;
+};
+
+const map4x4 =
+  "\
+↓↑↘→\n\
+←↑S↗\n\
+↖↓↓↙\n\
+←↓→↘";
+
+const map5x5 =
+  "\
+↖→↓←↗\n\
+↑←↓→↓\n\
+↑→S←↓\n\
+↑←↓→↓\n\
+↙→↑←↘";
+
+log(dance(map4x4));
+log(dance(map5x5));
--- misk/strobo.js
+++ misk/strobo.js
@@ -1,203 +1,181 @@
 const oddDecreaseMap = {
-    0: 2,
-    1: 1,
-    8: 0,
+  0: 2,
+  1: 1,
+  8: 0,
 };
 
 const evenDecreaseMap = {
-    "00": 4,
-    11: 3,
-    69: 2,
-    88: 1,
-    96: 0,
+  "00": 4,
+  11: 3,
+  69: 2,
+  88: 1,
+  96: 0,
 };
 
 const baseIndexes = {
-    0: 0,
-    1: 1,
-    8: 2,
-    11: 3,
-    69: 4,
-    88: 5,
-    96: 6,
+  0: 0,
+  1: 1,
+  8: 2,
+  11: 3,
+  69: 4,
+  88: 5,
+  96: 6,
 };
 
 const generateIndexesByStringSize = (n) => {
-    let counts = [0, 3, 4];
-    let indexes = [0, 2, 6];
+  let counts = [0, 3, 4];
+  let indexes = [0, 2, 6];
 
-    for (let i = 3; i <= n; i++) {
-        if (i % 2 === 0) {
-            counts[i] = counts[i - 2] * 5;
-        } else {
-            counts[i] = counts[i - 1] * 3;
-        }
-
-        indexes[i] = indexes[i - 1] + counts[i];
+  for (let i = 3; i <= n; i++) {
+    if (i % 2 === 0) {
+      counts[i] = counts[i - 2] * 5;
+    } else {
+      counts[i] = counts[i - 1] * 3;
     }
 
-    return indexes;
+    indexes[i] = indexes[i - 1] + counts[i];
+  }
+
+  return indexes;
 };
 
 const createIdx = (n) => {
-    const indexes = generateIndexesByStringSize(n);
+  const indexes = generateIndexesByStringSize(n);
 
-    const _createIdx = (num) => {
-        if (baseIndexes[num] !== undefined) {
-            return baseIndexes[num];
-        }
+  const _createIdx = (num) => {
+    if (baseIndexes[num] !== undefined) {
+      return baseIndexes[num];
+    }
 
-        const middle = num.length / 2;
-        let rest, nextNum, variants, decrease;
+    const middle = num.length / 2;
+    let rest, nextNum, variants, decrease;
 
-        if (num.length % 2 === 0) {
-            rest = num.slice(0, middle - 1) + num.slice(middle + 1);
-            nextNum = num.slice(middle - 1, middle + 1);
-            variants = 5;
-            decrease = evenDecreaseMap[nextNum];
-        } else {
-            rest = num.slice(0, middle) + num.slice(middle + 1);
-            nextNum = num.slice(middle, middle + 1);
-            variants = 3;
-            decrease = oddDecreaseMap[nextNum];
-        }
+    if (num.length % 2 === 0) {
+      rest = num.slice(0, middle - 1) + num.slice(middle + 1);
+      nextNum = num.slice(middle - 1, middle + 1);
+      variants = 5;
+      decrease = evenDecreaseMap[nextNum];
+    } else {
+      rest = num.slice(0, middle) + num.slice(middle + 1);
+      nextNum = num.slice(middle, middle + 1);
+      variants = 3;
+      decrease = oddDecreaseMap[nextNum];
+    }
 
-        const base = indexes[num.length - 1];
+    const base = indexes[num.length - 1];
 
-        const restPositionInOwnGroup =
-            _createIdx(rest) - indexes[rest.length - 1];
-        const result = base + restPositionInOwnGroup * variants - decrease;
-        return result;
-    };
+    const restPositionInOwnGroup = _createIdx(rest) - indexes[rest.length - 1];
+    const result = base + restPositionInOwnGroup * variants - decrease;
+    return result;
+  };
 
-    return _createIdx;
+  return _createIdx;
 };
 
-const singleRound = (num) => {
-    return (
-        (num <= "0" && "0") ||
-        (num <= "1" && "1") ||
-        (num <= "6" && "6") ||
-        (num <= "8" && "8") ||
-        "9"
-    );
-};
+const singleRound = (num) => (num <= "0" && "0") || (num <= "1" && "1") || (num <= "6" && "6") || (num <= "8" && "8") || "9";
 
-const singleRoundOdd = (num) => {
-    return (num <= "0" && "0") || (num <= "1" && "1") || "8";
-};
+const singleRoundOdd = (num) => (num <= "0" && "0") || (num <= "1" && "1") || "8";
 
-const singleIncr = (num) => {
-    return (num <= "0" && "1") || (num <= "1" && "8") || "8";
-};
+const singleIncr = (num) => (num <= "0" && "1") || (num <= "1" && "8") || "8";
 
-const doubleIncr = (num) => {
-    return (
-        (num <= "00" && "11") ||
-        (num <= "11" && "69") ||
-        (num <= "88" && "96") ||
-        "96"
-    );
-};
+const doubleIncr = (num) => (num <= "00" && "11") || (num <= "11" && "69") || (num <= "88" && "96") || "96";
 
-const getOppositeChar = (char) => {
-    return (char === "9" && "6") || (char === "6" && "9") || char;
-};
+const getOppositeChar = (char) => (char === "9" && "6") || (char === "6" && "9") || char;
 
 const generateNextStrobogramNumber = (num) => {
-    if (num === "0") {
-        return "0";
+  if (num === "0") {
+    return "0";
+  }
+
+  const nextLine = [];
+
+  let i = 0;
+  let j = num.length - 1;
+  let drop = false;
+
+  while (i < j) {
+    if (drop) {
+      nextLine[i] = "0";
+      nextLine[j] = "0";
+    } else {
+      const nextChar = singleRound(num[i]);
+      if (nextChar !== num[i]) {
+        drop = true;
+      }
+
+      nextLine[i] = nextChar;
+      nextLine[j] = getOppositeChar(nextChar);
     }
 
-    const nextLine = [];
+    i++;
+    j--;
+  }
 
-    let i = 0;
-    let j = num.length - 1;
-    let drop = false;
+  if (i === j) {
+    if (drop) {
+      nextLine[i] = "0";
+    } else {
+      nextLine[i] = singleRoundOdd(num[i]);
+    }
+  }
 
-    while (i < j) {
-        if (drop) {
-            nextLine[i] = "0";
-            nextLine[j] = "0";
+  if (num > nextLine.join("")) {
+    let i, j;
+
+    if (nextLine.length % 2 === 0) {
+      i = nextLine.length / 2 - 1;
+      j = nextLine.length / 2;
+    } else {
+      i = j = Math.floor(nextLine.length / 2);
+    }
+
+    let reminder = true;
+
+    while (reminder) {
+      if (i < 0 || j >= nextLine.length) break;
+
+      if (i === j) {
+        const nextChar = singleIncr(nextLine[i]);
+        if (nextChar === nextLine[i]) {
+          nextLine[i] = "0";
         } else {
-            const nextChar = singleRound(num[i]);
-            if (nextChar !== num[i]) {
-                drop = true;
-            }
-
-            nextLine[i] = nextChar;
-            nextLine[j] = getOppositeChar(nextChar);
+          reminder = false;
+          nextLine[i] = nextChar;
         }
-
-        i++;
-        j--;
-    }
-
-    if (i === j) {
-        if (drop) {
-            nextLine[i] = "0";
+      } else {
+        const char = nextLine[i] + nextLine[j];
+        const nextChar = doubleIncr(char);
+        if (nextChar === char) {
+          nextLine[i] = "0";
+          nextLine[j] = "0";
         } else {
-            nextLine[i] = singleRoundOdd(num[i]);
+          reminder = false;
+          nextLine[i] = nextChar[0];
+          nextLine[j] = nextChar[1];
         }
+      }
+
+      i--;
+      j++;
     }
+  }
 
-    if (num > nextLine.join("")) {
-        let i, j;
+  if (nextLine[0] === "0") {
+    nextLine[0] = "1";
+    nextLine.push("1");
+  }
 
-        if (nextLine.length % 2 === 0) {
-            i = nextLine.length / 2 - 1;
-            j = nextLine.length / 2;
-        } else {
-            i = j = Math.floor(nextLine.length / 2);
-        }
-
-        let reminder = true;
-
-        while (reminder) {
-            if (i < 0 || j >= nextLine.length) break;
-
-            if (i === j) {
-                const nextChar = singleIncr(nextLine[i]);
-                if (nextChar === nextLine[i]) {
-                    nextLine[i] = "0";
-                } else {
-                    reminder = false;
-                    nextLine[i] = nextChar;
-                }
-            } else {
-                const char = nextLine[i] + nextLine[j];
-                const nextChar = doubleIncr(char);
-                if (nextChar === char) {
-                    nextLine[i] = "0";
-                    nextLine[j] = "0";
-                } else {
-                    reminder = false;
-                    nextLine[i] = nextChar[0];
-                    nextLine[j] = nextChar[1];
-                }
-            }
-
-            i--;
-            j++;
-        }
-    }
-
-    if (nextLine[0] === "0") {
-        nextLine[0] = "1";
-        nextLine.push("1");
-    }
-
-    return nextLine.join("");
+  return nextLine.join("");
 };
 
 const upsideDown = (a, b) => {
-    const idx = createIdx(b.length);
+  const idx = createIdx(b.length);
 
-    const aIdx = idx(generateNextStrobogramNumber(a));
-    const bNext = generateNextStrobogramNumber(b);
+  const aIdx = idx(generateNextStrobogramNumber(a));
+  const bNext = generateNextStrobogramNumber(b);
 
-    let bIdx = idx(bNext);
-    bIdx = bNext !== b ? bIdx - 1 : bIdx;
+  let bIdx = idx(bNext);
+  bIdx = bNext !== b ? bIdx - 1 : bIdx;
 
-    return bIdx - aIdx + 1;
+  return bIdx - aIdx + 1;
 };
